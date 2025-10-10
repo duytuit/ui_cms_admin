19165b5ddd791770564a294e08285a417d601101
Duy Tu <duytu89@gmail.com>
2025-10-10 10:46:58 +0700

    dev

--- src/modules/bill/api/index.tsx
+++
@@ -1,7 +0,0 @@
-import { getData, postData } from "lib/request";
-
-export const listBill = (params:any) => getData("system/bill/list", params);
-export const deleteBill = (params:any) => postData("system/bill/delete", params);
-export const addBill = (params:any) => postData("system/bill/create", params);
-export const updateBill = (params:any) => postData("system/bill/update", params);
-export const updateStatusBill = (params:any) => postData("system/bill/update/status", params);
