5afde5ae8c5225e65391a09bd83e81d064eb9c22
Duy Tu <duytu89@gmail.com>
2026-01-02 00:30:41 +0700

    dev

--- src/layout/MenuSidebar.tsx
+++ src/layout/MenuSidebar.tsx
@@ -4,89 +4,130 @@ import { classNames } from 'primereact/utils';
 import { CSSTransition } from 'react-transition-group';
 import { useEffect, useState } from 'react';
 
-export default function MenuSidebar(props: any){
-    const employeeInfo = localStorage.getItem('employeeInfo') ? JSON.parse(localStorage.getItem('employeeInfo') || '{}') : null;
-    const location = useLocation();
-    const [activeMenu, setActiveMenu] = useState('');
-    const item = props.item;
-    const menuRender = [280].includes(employeeInfo?.user_id) ? props.item.items : Array.isArray(props.item.items) ? props.item.items.filter((x: any) => x.admin === false) : [];
-    const key = props.parentKey ? props.parentKey + '-' + props.index : String(props.index);
-    const active = activeMenu === key || (activeMenu && activeMenu.startsWith(key + '-')) || false;
-    const permissionTool = ['/','/page-one','/page-two','/auth/login','/categories/list','/bill/list','/customer/list',"/receipt/list",'/service/list','/product/list','/post/list','/category/post/list'];
-    const itemClick = (event:any) => {
-        //avoid processing disabled items
-        if (item.disabled) {
-            event.preventDefault();
-            return;
-        }
-        //execute command
-        if (item.command) {
-            item.command({ originalEvent: event, item: item });
-        }
-        // toggle active state
-        if (item.items) setActiveMenu(active ? props.parentKey : key);
-        else setActiveMenu(key);
-    };
+export default function MenuSidebar(props: any) {
+  const employeeInfo = JSON.parse(localStorage.getItem('employeeInfo') || '{}');
+  const isAdmin = [280].includes(employeeInfo?.user_id);
 
-    useEffect(() => {
-        let isShow = false;
-        if (item.items) {
-            item.items.forEach((i:any) => {
-                if (location.pathname.includes(i.route)) {
-                    isShow = true;
-                }
-            })
-        };
-        if (isShow) setActiveMenu(key);
-    }, []);
+  const location = useLocation();
+  const [activeMenu, setActiveMenu] = useState<string>('');
 
-    if (item.name && !item.route && item.items) {
-        let isDisabled = true;
-        item.items.forEach((item:any) => {
-            if (item.route && permissionTool.includes(item.route)) {
-                isDisabled = false;
-            }
-            // if (item.route) {
-            //     isDisabled = false;
-            // }
-        });
-        if (isDisabled) return <li key={props.index}></li>;
-    };
-    //if (item.route && !permissionTool.includes(item.route)) return <li key={props.index}></li>;
-    const subMenu = item.items && item.visible !== false && (
-        <CSSTransition timeout={{ enter: 1000, exit: 450 }} classNames="layout-submenu" in={props.root ? true : active} key={item.name}>
-            <ul>
-                {menuRender.map((child:any, i:number) => {
-                    return <MenuSidebar item={child} index={i} className={child.badgeClass} parentKey={key} key={child.name + '-' + i} />;
-                })}
-            </ul>
-        </CSSTransition>
+  const item = props.item;
+
+  const key = props.parentKey
+    ? `${props.parentKey}-${props.index}`
+    : `${props.index}`;
+
+  /* ===== active luôn là boolean ===== */
+  const active: boolean =
+    activeMenu === key ||
+    (activeMenu.length > 0 && activeMenu.startsWith(key + '-'));
+
+  /* =================================================
+     CHỈ FILTER Ở MENU CHA
+     ================================================= */
+  const menuRender = Array.isArray(item.items)
+    ? item.items
+    : [];
+
+  /* ===== click menu ===== */
+  const itemClick = (event: any) => {
+    if (item.disabled) {
+      event.preventDefault();
+      return;
+    }
+
+    if (item.command) {
+      item.command({ originalEvent: event, item });
+    }
+
+    if (item.items) {
+      setActiveMenu(active ? props.parentKey : key);
+    } else {
+      setActiveMenu(key);
+    }
+  };
+
+  /* ===== auto mở menu theo route ===== */
+  useEffect(() => {
+    if (!Array.isArray(item.items)) return;
+
+    const matched = item.items.some((i: any) =>
+      location.pathname.startsWith(i.route)
     );
 
-    return (
-        <li className={classNames({ 'layout-root-menuitem': props.root, 'active-menuitem': active })}>
-            {/* {props.root && item.visible !== false && <div className="layout-menuitem-root-text">{item.name}</div>} */}
+    if (matched) setActiveMenu(key);
+  }, []);
 
-            {(!item.route || item.items) && item.visible !== false ? (
-                <Link to={''} onClick={(e) => itemClick(e)} className={classNames(item.class, 'p-ripple')} tabIndex={0}>
-                    <i className={classNames('layout-menuitem-icon', item.icon)}></i>
-                    <span className="layout-menuitem-text">{item.name}</span>
-                    {item.items && <i className="pi pi-fw pi-angle-down layout-submenu-toggler"></i>}
-                    <Ripple />
-                </Link>
-            ) : null}
+  /* =================================================
+     ẨN MENU CHA admin:true với USER THƯỜNG
+     ================================================= */
+  if (item.admin === true && !isAdmin) {
+    return null;
+  }
 
-            {item.route && !item.items && item.visible !== false ? (
-                <Link to={item.route} replace={item.replaceUrl} onClick={(e) => itemClick(e)}
-                    className={classNames(item.class, 'p-ripple', { 'active-item': location.pathname.includes(item.route + '/') || location.pathname === item.route })} tabIndex={0}>
-                    <i className={classNames('layout-menuitem-icon', item.icon)}></i>
-                    <span className={classNames('layout-menuitem-text', { 'submenu-text': !item.icon })}>{item.name}</span>
-                    {item.items && <i className="pi pi-fw pi-angle-down layout-submenu-toggler"></i>}
-                    <Ripple />
-                </Link>
-            ) : null}
-
-            {subMenu}
-        </li>
+  /* ===== submenu ===== */
+  const subMenu =
+    menuRender.length > 0 && item.visible !== false && (
+      <CSSTransition
+        in={props.root ? true : active}
+        timeout={{ enter: 300, exit: 200 }}
+        classNames="layout-submenu"
+        unmountOnExit
+      >
+        <ul>
+          {menuRender.map((child: any, i: number) => (
+            <MenuSidebar
+              key={(child.name || 'menu') + '-' + i}
+              item={child}
+              index={i}
+              parentKey={key}
+            />
+          ))}
+        </ul>
+      </CSSTransition>
     );
-};
+
+  return (
+    <li
+      className={classNames({
+        'layout-root-menuitem': props.root,
+        'active-menuitem': active
+      })}
+    >
+      {/* ===== menu cha ===== */}
+      {(!item.route || item.items) && item.visible !== false && (
+        <Link
+          to=""
+          onClick={itemClick}
+          className={classNames(item.class, 'p-ripple')}
+        >
+          <i className={classNames('layout-menuitem-icon', item.icon)} />
+          <span className="layout-menuitem-text">{item.name}</span>
+          {item.items && (
+            <i className="pi pi-fw pi-angle-down layout-submenu-toggler" />
+          )}
+          <Ripple />
+        </Link>
+      )}
+
+      {/* ===== menu lá ===== */}
+      {item.route && !item.items && item.visible !== false && (
+        <Link
+          to={item.route}
+          onClick={itemClick}
+          className={classNames(item.class, 'p-ripple', {
+            'active-item':
+              location.pathname === item.route ||
+              location.pathname.startsWith(item.route + '/')
+          })}
+        >
+          <i className={classNames('layout-menuitem-icon', item.icon)} />
+          <span className="layout-menuitem-text">{item.name}</span>
+          <Ripple />
+        </Link>
+      )}
+
+      {subMenu}
+    </li>
+  );
+}
